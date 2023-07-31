ba2e535d34383039314fda6e3eba257faff64836
Komilov Dilshod <komilovd@silkroadprofessionals.com>
2023-07-31 15:10:08 +0500

    fix and set up

--- src/rental/Rental.Api/WeatherForecast.cs
+++ src/rental/Rental.Api/WeatherForecast.cs
@@ -8,6 +8,6 @@ namespace Rental.Api
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public string Summary { get; set; }
     }
 }
