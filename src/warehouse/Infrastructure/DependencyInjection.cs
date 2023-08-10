993e04d8fb7b1eec98e978d60bbf78b8ca40ecc8
Nematulloh <nematulloh111@gmail.com>
2023-08-10 20:10:42 +0500

    add cinfig

---
+++ src/warehouse/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+            return services;
+        }
+    }
+}
