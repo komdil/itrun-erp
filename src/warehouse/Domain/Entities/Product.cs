1a3d73785031c0254a08969bcf989b6355a900ca
Ben <hotblood.boy9@gmail.com>
2023-08-28 02:43:42 -0700

    Add Product Entity

---
+++ src/warehouse/Domain/Entities/Product.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public ProductUOM Uom { get; set; }
+        public string Category {get; set;}
+        public string Manufacturer {get; set;}
+        public decimal Price { get; set;}
+        public string Description {get; set;}
+        public int Quantity { get; set;}
+    }
+}
