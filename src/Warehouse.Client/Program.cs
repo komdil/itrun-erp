a264650ce369d768d1a4393784decda780c0db9b
Komilov Dilshod <komilovd@silkroadprofessionals.com>
2023-09-14 19:01:17 +0500

    fix import issues

--- src/Warehouse.Client/Program.cs
+++ src/Warehouse.Client/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TabBlazor;
-using TabBlazor.Templates.Wasm;
+using Warehouse.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
