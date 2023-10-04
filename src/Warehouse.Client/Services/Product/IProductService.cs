26c1d954abf13f09bfbaac1de3d7833db1276959
Ben <benazir.rajabov@gmail.com>
2023-10-04 07:55:57 -0700

    Finish Blazorside product service

--- src/Warehouse.Client/Services/Product/IProductService.cs
+++ src/Warehouse.Client/Services/Product/IProductService.cs
@@ -1,15 +1,17 @@
 ﻿using Warehouse.Client.Services;
 using Warehouse.Contracts.Product;
 
-namespace Warehouse.Client;
-
-public interface IProductService
+namespace Warehouse.Client.Services.Product
 {
-    Task<ApiResponse<List<SingleProductResponse>>> GetAllAsync(GetSingleProductQuery getProductQuery);
 
-    Task<ApiResponse<SingleProductResponse>> CreateAsync(CreateProductRequest request);
+    public interface IProductService
+    {
+        Task<ApiResponse<List<SingleProductResponse>>> GetAllAsync(GetProductsQuery getProductQuery);
 
-    Task<ApiResponse<SingleProductResponse>> UpdateAsync(UpdateProductRequest request);
+        Task<ApiResponse<SingleProductResponse>> CreateAsync(CreateProductRequest request);
 
-    Task<ApiResponse> DeleteAsync(DeleteProductRequest request);
+        Task<ApiResponse<SingleProductResponse>> UpdateAsync(UpdateProductRequest request);
+
+        Task<ApiResponse> DeleteAsync(DeleteProductRequest request);
+    }
 }
--- src/Warehouse.Client/Services/Product/ProductService.cs
+++ src/Warehouse.Client/Services/Product/ProductService.cs
@@ -1,36 +1,36 @@
-﻿using Warehouse.Client.Services;
-using Warehouse.Client.Services.HttpClients;
+﻿using Warehouse.Client.Services.HttpClients;
 using Warehouse.Contracts.Product;
 
-namespace Warehouse.Client;
-
-public class ProductService : IProductService
+namespace Warehouse.Client.Services.Product
 {
-    private readonly IHttpClientService _httpClient;
-    private readonly IConfiguration _configuration;
-    public ProductService(IHttpClientService httpClient, IConfiguration configuration)
+    public class ProductService : IProductService
     {
-        _httpClient = httpClient;
-        _configuration = configuration;
-    }
+        private readonly IHttpClientService _httpClient;
+        private readonly IConfiguration _configuration;
+        public ProductService(IHttpClientService httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+        }
 
-    public async Task<ApiResponse<List<SingleProductResponse>>> GetAllAsync(GetSingleProductQuery getProductQuery)
-    {
-        return await _httpClient.GetAsJsonAsync<List<SingleProductResponse>>($"{_configuration["WarehouseServiceUrl"]}/products", getProductQuery);
-    }
+        public async Task<ApiResponse<List<SingleProductResponse>>> GetAllAsync(GetProductsQuery getProductQuery)
+        {
+            return await _httpClient.GetAsJsonAsync<List<SingleProductResponse>>($"{_configuration["WarehouseServiceUrl"]}/products", getProductQuery);
+        }
 
-    public async Task<ApiResponse<SingleProductResponse>> CreateAsync(CreateProductRequest request)
-    {
-        return await _httpClient.PostAsJsonAsync<SingleProductResponse>($"{_configuration["WarehouseServiceUrl"]}/products", request);
-    }
+        public async Task<ApiResponse<SingleProductResponse>> CreateAsync(CreateProductRequest request)
+        {
+            return await _httpClient.PostAsJsonAsync<SingleProductResponse>($"{_configuration["WarehouseServiceUrl"]}/products", request);
+        }
 
-    public async Task<ApiResponse> DeleteAsync(DeleteProductRequest request)
-    {
-        return await _httpClient.DeleteAsync($"{_configuration["WarehouseServiceUrl"]}/products/{request.Slug}");
-    }
+        public async Task<ApiResponse> DeleteAsync(DeleteProductRequest request)
+        {
+            return await _httpClient.DeleteAsync($"{_configuration["WarehouseServiceUrl"]}/products/{request.Slug}");
+        }
 
-    public async Task<ApiResponse<SingleProductResponse>> UpdateAsync(UpdateProductRequest request)
-    {
-        return await _httpClient.PutAsJsonAsync<SingleProductResponse>($"{_configuration["WarehouseServiceUrl"]}/products/{request.Id}", request);
+        public async Task<ApiResponse<SingleProductResponse>> UpdateAsync(UpdateProductRequest request)
+        {
+            return await _httpClient.PutAsJsonAsync<SingleProductResponse>($"{_configuration["WarehouseServiceUrl"]}/products/{request.Id}", request);
+        }
     }
 }
