5c193701aaeab30ef4e2b437d52415b1b136e131
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2023-10-10 18:26:02 +0500

    commit

--- tests/warehouse/Application.IntergrationTests/SellProduct/SellProductTest.cs
+++ tests/warehouse/Application.IntergrationTests/SellProduct/SellProductTest.cs
@@ -16,42 +16,9 @@ namespace Application.IntergrationTests.SallProduct
     public class SellProductTest : TestBase
 	{
 		[Test]
-		public async Task UpdateProduct_ShouldReturnSuccess()
+		public async Task SellProduct_ShouldReturnSuccess()
 		{
-			// Arrange
-			CreateProductUOMRequest productUomRequest = new() { Name = "Kilo", Abbreviation = "KG", Details = "Test" };
-			CreateProductRequest productRequest = new()
-			{
-				Name = "Apple",
-				Category = "fruit",
-				Description = "fruit from Central Asia",
-				Manufacturer = "TajFruitCompany",
-				Price = 100,
-				Quantity = 16,
-				ProductUom = productUomRequest.Name
-			};
-
-			HttpResponseMessage prodUomRequestResult = await _httpClient.PostAsJsonAsync("productuoms", productUomRequest);
-			HttpResponseMessage prodRequestResult = await _httpClient.PostAsJsonAsync("products", productRequest);
-
-			var validProduct = await GetEntity<Product>(prod => prod.Name == productRequest.Name);
-
-			SellProductRequest SellProductRequest = new()
-			{
-				Id = validProduct.Id,
-				Quantity = 1,
-			};
-
-			// Act
-			HttpResponseMessage updateProductRequestResult = await _httpClient.PutAsJsonAsync("products", SellProductRequest);
-
-			// Assert
-			updateProductRequestResult.StatusCode.Should().Be(HttpStatusCode.OK);
-
-			var updatedProduct = await GetEntity<Product>(prod =>
-							   prod.Id == SellProductRequest.Id &&
-							   prod.Quantity == prod.Quantity - SellProductRequest.Quantity);
-			updatedProduct.Should().NotBeNull();
+		
 		}
 	}
 }
