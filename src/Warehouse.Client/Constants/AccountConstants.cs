3fd02a5b345ab74026b66e4fef57755c924fd70c
Komilov Dilshod <komilovd@silkroadprofessionals.com>
2023-10-20 08:32:10 +0500

    finish authroization on client side

--- src/Warehouse.Client/Constants/AccountConstants.cs
+++ src/Warehouse.Client/Constants/AccountConstants.cs
@@ -5,5 +5,8 @@
         public const string SuperAdminRoleName = "SuperAdmin";
         public const string SuperAdminUserName = "SuperAdmin";
         public const string SuperAdminPolicy = "SuperAdmin";
+
+        public const string BuyerRoles = "SuperAdmin,Buyer";
+        public const string SellerRoles = "SuperAdmin,Seller";
     }
 }
